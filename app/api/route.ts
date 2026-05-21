d08fd76fb957d254948233a86c629e01813e2a0c
locifer18 <anshkanda08@gmail.com>
2026-05-21 19:00:22 +0530

    add

---
+++ app/api/route.ts
@@ -0,0 +1,16 @@
+export function GET(){
+    try{
+        
+        const user = db.user.findAll({
+            include: {
+                name: true,
+                email: true,
+                phone: true,
+            }
+        })
+
+
+    } catch{
+
+    }
+}
